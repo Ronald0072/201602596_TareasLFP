f21c42bbed94b22e5b50f81fcdb84b11fa68718b
unknown <3121727530413@ingenieria.usac.edu.gt git config --global user.email 3121727530413@ingenieria.usac.edu.gt>
2020-08-31 23:10:39 -0600

    Tarea 3 modificada

--- Tarea 3/index.py
+++ Tarea 3/index.py
@@ -30,7 +30,7 @@ def quemado():
 
 def html(tablas):
     ht="""<!doctype html>
-        <html lang="en">
+        <html lang="es">
         <head>
             <meta charset="utf-8">
             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -74,7 +74,8 @@ def crear_html(texto):
     file = open("registros.html", "w")
     file.write(texto)
     file.close()
-    print("creado")
+    os.system("registros.html")
+    
 
 def main():
     quemado()
